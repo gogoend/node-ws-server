3a19a37f7c3b5fa2a31183a4cbf7a071425035f1
gogoend <gogoend@qq.com>
2020-12-21 22:22:18 +0800

    update: 去掉无用的测试代码

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ expressWs(app)
 
 app.ws('/ws', (ws, req) => {
     ws.send(`${new Date().toDateString()} 连接成功`)
-    let intetval
-    interval = setInterval(() => {
-        if (ws.readyState === ws.OPEN) {
-            ws.send(Math.random().toFixed(2))
-        } else {
-            clearInterval(intetval)
-        }
-    },2000)
 
     ws.on('message', msg => {
         ws.send(`${new Date().toDateString()} ${msg}`)
